1852f7eb25e0cfc3790b304ff6bf4c44e107b43b
Nariman <66928404+nariman1993@users.noreply.github.com>
2020-07-08 18:00:42 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,37 @@
+
+
+
+$('#dark-btn').click(function(e){
+    
+    $('.sections').removeClass('default-bg extra-bg extra-bg-2');
+
+
+    $('body').toggleClass('dark');
+    $('.about-me').toggleClass('dark');
+    $('.my-skills').toggleClass('dark');
+    $('#dark-btn').addClass('dark-btn-2');
+
+    if(e.currentTarget.innerHTML == 'DARK: ON'){
+        $('#dark-btn').text('DARK: OFF');
+    }
+    else{
+        $('#dark-btn').text('DARK: ON');
+    }
+
+});
+
+
+$('#all').click(function(){
+    $('.works').slideDown(300);
+});
+
+
+$('#front').click(function(){
+    $('.front').slideDown(500);
+    $('.back').slideUp(500);
+});
+
+$('#back').click(function(){;
+    $('.back').slideDown(500);
+    $('.front').slideUp(500)
+});
